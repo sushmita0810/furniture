9639fbcfc80c1a88784005a38dac21c888b8f997
sushmita0810 <35845477+sushmita0810@users.noreply.github.com>
2022-07-24 11:25:15 +0530

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -1,6 +1,8 @@
 package hackerrank;
 
 import java.util.HashMap;
+import hackerrank.Furniture;
+import hackerrank.FurnitureOrderInterface;
 
 public class FurnitureOrder implements FurnitureOrderInterface {
     /**
@@ -10,36 +12,56 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
-    FurnitureOrder() {
-        // TODO: Complete the constructor
+      HashMap<Furniture,Integer> hm = new HashMap<>();
+	FurnitureOrder() {
+        // TODO: Complete the constructorg
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+    	if(hm.containsKey(type))hm.put(type, hm.get(type)+furnitureCount);
+    	else hm.put(type, furnitureCount);
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+//    	HashMap<Furniture, Integer> hm2 = hm;
+        return hm;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+    	float sum = 0 ;
+    	for(Furniture i : hm.keySet())
+    	{
+    		if(i==Furniture.CHAIR)sum+=hm.get(Furniture.CHAIR)*100.0f;
+    		else if(i==Furniture.TABLE)sum+=hm.get(Furniture.TABLE)*250.0f;
+    		else if(i==Furniture.COUCH)sum+=hm.get(Furniture.COUCH)*500.0f;
+
+    	}
+        return sum;
     }
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+        return hm.get(type);
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+        return type.cost();
     }
 
     public int getTotalOrderQuantity() {
         // TODO: Complete the method
-        return -1;
+    	int count  = 0 ;
+    	for(Furniture i : hm.keySet())
+    	{
+    		if(i==Furniture.CHAIR)count+=hm.get(Furniture.CHAIR);
+    		else if(i==Furniture.TABLE)count+=hm.get(Furniture.TABLE);
+    		else if(i==Furniture.COUCH)count+=hm.get(Furniture.COUCH);
+
+    	}
+        return count;
     }
 }
